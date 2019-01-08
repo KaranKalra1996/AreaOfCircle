8540a8757c8a544bb40c1b99d868ac434975a6c8
Karan Kalra <karankalra06.kk@gmail.com>
2019-01-08 11:05:33 +0530

    addeing new files

---
+++ oddEven.py
@@ -0,0 +1,5 @@
+number = int(input("Give number : "))
+if number % 2 == 0 :
+	print(str(number)+" is even.")
+else :
+	print(str(number)+" is odd.")
---
+++ turning100.py
@@ -0,0 +1,9 @@
+currentAge = int(input("Input current age : "))
+currentYear = 2018
+yearTurning100 = 0
+if currentAge == 100 :
+	print("Already 100")
+else :
+	yearTurning100 = currentYear + 100 - currentAge
+	print(yearTurning100)
+ 
